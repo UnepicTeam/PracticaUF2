346461476c93f9b66f1d48b1253a5b05ea8d88bd
Talessa <mjunoygomez@gmail.com>
2018-05-04 17:11:02 +0200

    Initial commit

--- src/com/UF2/Proyecto/Mjunoy/Main.java
+++ src/com/UF2/Proyecto/Mjunoy/Main.java
@@ -1,20 +1,10 @@
 package com.UF2.Proyecto.Mjunoy;
 
-import com.UF2.Proyecto.Mjunoy.manager.ManagerVeterianarios;
-import com.UF2.Proyecto.Mjunoy.model.Administrador;
-import com.UF2.Proyecto.Mjunoy.model.Tienda;
-import com.UF2.Proyecto.Mjunoy.model.Veterinario;
 import com.UF2.Proyecto.Mjunoy.view.PantallaPrincipal;
-
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        Database db = Database.get();
-
         new PantallaPrincipal().iniciar();
 
     }
